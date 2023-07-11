cf5389988059a5b9e9a144c98bed11f9264518db
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-07-10 21:02:20 -0300

    feat: COR liberate for front

--- src/main/java/br/com/ceslab/ceslab/resources/DegreeResource.java
+++ src/main/java/br/com/ceslab/ceslab/resources/DegreeResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "https://ceslab.com.br")
 @RestController
 @RequestMapping("/api/degree")
 public class DegreeResource {
