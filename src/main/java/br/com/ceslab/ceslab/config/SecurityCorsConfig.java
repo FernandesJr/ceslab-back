c18c6c539e390942621671b79e36917e5c1d9a9b
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-08-11 15:01:25 -0300

    feat: configuration CORS

---
+++ src/main/java/br/com/ceslab/ceslab/config/SecurityCorsConfig.java
@@ -0,0 +1,34 @@
+package br.com.ceslab.ceslab.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityCorsConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        String[] ORIGIN_CORS = new String[2];
+        ORIGIN_CORS[0] = "https://ceslab.com.br";
+
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test"))
+            ORIGIN_CORS[1] = "http://localhost:4200";
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(ORIGIN_CORS);
+            }
+        };
+    }
+}
--- src/main/java/br/com/ceslab/ceslab/resources/DegreeResource.java
+++ src/main/java/br/com/ceslab/ceslab/resources/DegreeResource.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "https://ceslab.com.br")
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/degree")
 public class DegreeResource {
