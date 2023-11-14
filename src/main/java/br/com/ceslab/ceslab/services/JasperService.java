21a4c3a89da068a377bae450416006308364905e
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-11-14 13:37:34 -0300

    fix: class to found jasper in the project

--- src/main/java/br/com/ceslab/ceslab/services/JasperService.java
+++ src/main/java/br/com/ceslab/ceslab/services/JasperService.java
@@ -5,11 +5,12 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +21,9 @@ public class JasperService {
     @Autowired
     private Connection connection;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     private static final String DIRECTORY_JASPER = "classpath:jasper/";
 
     private Map<String, Object> params = new HashMap<>();
@@ -28,11 +32,14 @@ public class JasperService {
         this.params.put("PAYMENT_VOUCHER_ID", idPaymentVoucher);
         this.params.put("DIRECTORY_IMAGES", DIRECTORY_JASPER);
         try {
-            File file = ResourceUtils.getFile(DIRECTORY_JASPER + "payment_voucher.jasper");
-            JasperPrint print = JasperFillManager.fillReport(file.getAbsolutePath(), params, connection);
+            Resource resource = resourceLoader.getResource(DIRECTORY_JASPER + "payment_voucher.jasper");
+            InputStream stream = resource.getInputStream();
+            JasperPrint print = JasperFillManager.fillReport(stream, params, connection);
            return JasperExportManager.exportReportToPdf(print);
         } catch (FileNotFoundException | JRException e) {
             e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
         return null;
     }
